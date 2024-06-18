2b2a0ccdb449d425d07f069fa4223c73c56343b0
Petr (Desktop) <petrbohac3@seznam.cz>
2024-06-19 00:57:01 +0200

    tf

---
+++ proxmox/terraform/provider.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "Telmate/proxmox"
+      version = "3.0.1-rc3"
+    }
+  }
+}
+
+variable "proxmox_api_url" {
+  type = string
+  default = "https://pve1.pycrs.cz/api2/json"
+}
+
+variable "proxmox_api_token_id" {
+  type = string
+  sensitive = true
+}
+
+variable "proxmox_api_token_secret" {
+  type = string
+  sensitive = true
+}
+
+provider "proxmox" {
+  pm_api_url = var.proxmox_api_url
+  pm_api_token_id = var.proxmox_api_token_id
+  pm_api_token_secret = var.proxmox_api_token_secret
+}
