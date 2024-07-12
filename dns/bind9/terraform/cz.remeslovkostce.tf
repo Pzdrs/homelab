f2a7390fa63423fc281120c78edb3d405ec55ee9
Petr (WSL) <petrbohac3@seznam.cz>
2024-07-12 16:34:37 +0200

    ttl local var

--- dns/bind9/terraform/cz.remeslovkostce.tf
+++ dns/bind9/terraform/cz.remeslovkostce.tf
@@ -1,20 +1,24 @@
+locals {
+  ttl = 86400
+}
+
 resource "dns_a_record_set" "root" {
   addresses = ["10.0.0.25"]
   name      = null
-  ttl       = 86400
+  ttl       = local.ttl
   zone      = "remeslovkostce.cz."
 }
 
 resource "dns_cname_record" "api" {
   cname = "remeslovkostce.cz."
   name  = "api"
-  ttl   = 86400
+  ttl   = local.ttl
   zone  = "remeslovkostce.cz."
 }
 
 resource "dns_cname_record" "www" {
   cname = "remeslovkostce.cz."
   name  = "www"
-  ttl   = 86400
+  ttl   = local.ttl
   zone  = "remeslovkostce.cz."
 }
